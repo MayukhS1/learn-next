b059372d16369eae7eef7a32076683718e51a849
MayukhS1 <mayukhsasmal1999@gmail.com>
2025-06-27 21:39:57 +0530

    path grouping

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+const ForgotPasswordPage = () => {
+    return (
+        <div>
+            <h1>Forgot Password Page</h1>
+            <p>This is the forgot password page.</p>
+        </div>
+    );
+}
+export default ForgotPasswordPage;
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>This is the Login page.</p>
+        </div>
+    );
+}
+export default LoginPage;
